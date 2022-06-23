79f27ff23a24c9e505fd625ca8e9410a10533da4
abhinav <99832265+Abhinav80555@users.noreply.github.com>
2022-06-23 06:00:00 +0000

    admin final think so

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 import {
   BrowserRouter as Router,
   Routes,
@@ -25,6 +28,10 @@ function App() {
             <Route path="/users" element={<UserList/>} />
             <Route path="/user/:userId" element={<User/>}/>
            <Route path="/newUser" element={<NewUser/>}/>
+           <Route path="/products" element={<ProductList />}/>
+           <Route path="/product/:productId" element={<Product />}/>
+           <Route path="/newproduct" element={<NewProduct />}/>
+            
 
 
 
