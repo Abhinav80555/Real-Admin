6cebf668df437799a6a0ab9bcc73b1868ed011d3
abhinav <99832265+Abhinav80555@users.noreply.github.com>
2022-06-25 05:27:33 +0000

    before finally admin

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -23,13 +23,15 @@ export default function WidgetLg() {
     <div className="widgetLg">
       <h3 className="widgetLgTitle">Latest transactions</h3>
       <table className="widgetLgTable">
+        <thead>
         <tr className="widgetLgTr">
           <th className="widgetLgTh">Customer</th>
           <th className="widgetLgTh">Date</th>
           <th className="widgetLgTh">Amount</th>
           <th className="widgetLgTh">Status</th>
-        </tr>
+        </tr></thead><tbody>
         {orders.map((order) => (
+      
           <tr className="widgetLgTr" key={order._id}>
             <td className="widgetLgUser">
               <span className="widgetLgName">{order.userId}</span>
@@ -40,7 +42,7 @@ export default function WidgetLg() {
               <Button type={order.status} />
             </td>
           </tr>
-        ))}
+        ))}</tbody>
       </table>
     </div>
   );
--- src/requestMethods.js
+++ src/requestMethods.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://API.abhinav80555.repl.co/api/";
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
-console.log(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken)
+
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
